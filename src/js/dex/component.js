81c8b35f26924453303859a3c425c89004d26a7e
Leslie David Tang <37604240+lesliedavidtang@users.noreply.github.com>
2018-07-05 16:32:05 +0800

    Made changs to the exchange pairs
    - there's a bug regarding KDEX, it could be due to the fact that KDEX does not has an asset id.
    - when exchanging KDEX, the pair would randomly switch between KDEX/other pair

--- src/js/dex/component.js
+++ src/js/dex/component.js
@@ -26,156 +26,156 @@
 
         emptyDataFields();
 
-        // var favoritePairs = [
-        //     // { amountAsset: Currency.KDEX, priceAsset: Currency.CKR },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.HOTX },
-        //     // { amountAsset: Currency.KDEX, priceAsset: Currency.DC },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.KDEX, priceAsset: Currency.VEX },
-        //     // break
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.KDEX }, // technically should work, but the display seems to be conky
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.IKI, priceAsset: Currency.VEX },   
-        //     // break
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.SGD, priceAsset: Currency.VEX }, 
-        //     // break
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.HOTX, priceAsset: Currency.VEX },    
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.KLTC, priceAsset: Currency.VEX },        
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.KETH, priceAsset: Currency.VEX },    
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.KBCH, priceAsset: Currency.VEX },        
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.KBTC, priceAsset: Currency.VEX },  
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.VAI },
-        //     { amountAsset: Currency.KWAVES, priceAsset: Currency.VEX }, 
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.VAI, priceAsset: Currency.VEX },
-        //     // copy last, replace with next asset
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.KDEX },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.IKI },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.SGD },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.HOTX },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.KLTC },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.KETH },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.KBCH },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.KBTC },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.KWAVES },
-        //     { amountAsset: Currency.VEX, priceAsset: Currency.VAI },                                              
-        // ];
-        var assetList = [
-           Currency.KDEX,
-           Currency.IKI,
-           Currency.SGD,
-           Currency.HOTX,
-           Currency.KLTC,
-           Currency.KETH,
-           Currency.KBCH,
-           Currency.KBTC,
-           Currency.KWAVES,
-           Currency.VAI,
-           Currency.VEX
+        var favoritePairs = [
+            // { amountAsset: Currency.KDEX, priceAsset: Currency.CKR },
+            { amountAsset: Currency.KDEX, priceAsset: Currency.SGD },
+            { amountAsset: Currency.KDEX, priceAsset: Currency.IKI },
+            { amountAsset: Currency.KDEX, priceAsset: Currency.HOTX },
+            // { amountAsset: Currency.KDEX, priceAsset: Currency.DC },
+            // { amountAsset: Currency.KDEX, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.KDEX, priceAsset: Currency.KETH },
+            { amountAsset: Currency.KDEX, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.KDEX, priceAsset: Currency.KBTC },
+            // { amountAsset: Currency.KDEX, priceAsset: Currency.KWAVES },
+            // { amountAsset: Currency.KDEX, priceAsset: Currency.VAI },
+            // { amountAsset: Currency.KDEX, priceAsset: Currency.VEX },
+            // break
+            { amountAsset: Currency.IKI, priceAsset: Currency.KDEX }, // technically should work, but the display seems to be conky
+            { amountAsset: Currency.IKI, priceAsset: Currency.SGD },
+            // { amountAsset: Currency.IKI, priceAsset: Currency.HOTX },
+            // { amountAsset: Currency.IKI, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.IKI, priceAsset: Currency.KETH },
+            { amountAsset: Currency.IKI, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.IKI, priceAsset: Currency.KBTC },
+            // { amountAsset: Currency.IKI, priceAsset: Currency.KWAVES },
+            // { amountAsset: Currency.IKI, priceAsset: Currency.VAI },
+            // { amountAsset: Currency.IKI, priceAsset: Currency.VEX },   
+            // break
+            { amountAsset: Currency.SGD, priceAsset: Currency.KDEX },
+            { amountAsset: Currency.SGD, priceAsset: Currency.IKI },
+            { amountAsset: Currency.SGD, priceAsset: Currency.HOTX },
+            { amountAsset: Currency.SGD, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.SGD, priceAsset: Currency.KETH },
+            { amountAsset: Currency.SGD, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.SGD, priceAsset: Currency.KBTC },
+            { amountAsset: Currency.SGD, priceAsset: Currency.KWAVES },
+            { amountAsset: Currency.SGD, priceAsset: Currency.VAI },
+            { amountAsset: Currency.SGD, priceAsset: Currency.VEX }, 
+            // break
+            { amountAsset: Currency.HOTX, priceAsset: Currency.KDEX },
+            // { amountAsset: Currency.HOTX, priceAsset: Currency.IKI },
+            { amountAsset: Currency.HOTX, priceAsset: Currency.SGD },
+            // { amountAsset: Currency.HOTX, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.HOTX, priceAsset: Currency.KETH },
+            { amountAsset: Currency.HOTX, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.HOTX, priceAsset: Currency.KBTC },
+            // { amountAsset: Currency.HOTX, priceAsset: Currency.KWAVES },
+            // { amountAsset: Currency.HOTX, priceAsset: Currency.VAI },
+            // { amountAsset: Currency.HOTX, priceAsset: Currency.VEX },    
+            // copy last, replace with next asset
+            { amountAsset: Currency.KLTC, priceAsset: Currency.KDEX },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.IKI },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.SGD },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.HOTX },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.KETH },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.KBTC },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.KWAVES },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.VAI },
+            { amountAsset: Currency.KLTC, priceAsset: Currency.VEX },        
+            // copy last, replace with next asset
+            { amountAsset: Currency.KETH, priceAsset: Currency.KDEX },
+            { amountAsset: Currency.KETH, priceAsset: Currency.IKI },
+            { amountAsset: Currency.KETH, priceAsset: Currency.SGD },
+            { amountAsset: Currency.KETH, priceAsset: Currency.HOTX },
+            { amountAsset: Currency.KETH, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.KETH, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.KETH, priceAsset: Currency.KBTC },
+            { amountAsset: Currency.KETH, priceAsset: Currency.KWAVES },
+            { amountAsset: Currency.KETH, priceAsset: Currency.VAI },
+            { amountAsset: Currency.KETH, priceAsset: Currency.VEX },    
+            // copy last, replace with next asset
+            { amountAsset: Currency.KBCH, priceAsset: Currency.KDEX },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.IKI },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.SGD },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.HOTX },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.KETH },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.KBTC },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.KWAVES },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.VAI },
+            { amountAsset: Currency.KBCH, priceAsset: Currency.VEX },        
+            // copy last, replace with next asset
+            { amountAsset: Currency.KBTC, priceAsset: Currency.KDEX },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.IKI },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.SGD },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.HOTX },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.KETH },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.KWAVES },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.VAI },
+            { amountAsset: Currency.KBTC, priceAsset: Currency.VEX },  
+            // copy last, replace with next asset
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.KDEX },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.IKI },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.SGD },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.HOTX },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.KETH },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.KBTC },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.VAI },
+            { amountAsset: Currency.KWAVES, priceAsset: Currency.VEX }, 
+            // copy last, replace with next asset
+            { amountAsset: Currency.VAI, priceAsset: Currency.KDEX },
+            // { amountAsset: Currency.VAI, priceAsset: Currency.IKI },
+            { amountAsset: Currency.VAI, priceAsset: Currency.SGD },
+            // { amountAsset: Currency.VAI, priceAsset: Currency.HOTX },
+            // { amountAsset: Currency.VAI, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.VAI, priceAsset: Currency.KETH },
+            { amountAsset: Currency.VAI, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.VAI, priceAsset: Currency.KBTC },
+            // { amountAsset: Currency.VAI, priceAsset: Currency.KWAVES },
+            // { amountAsset: Currency.VAI, priceAsset: Currency.VEX },
+            // copy last, replace with next asset
+            { amountAsset: Currency.VEX, priceAsset: Currency.KDEX },
+            // { amountAsset: Currency.VEX, priceAsset: Currency.IKI },
+            { amountAsset: Currency.VEX, priceAsset: Currency.SGD },
+            // { amountAsset: Currency.VEX, priceAsset: Currency.HOTX },
+            // { amountAsset: Currency.VEX, priceAsset: Currency.KLTC },
+            { amountAsset: Currency.VEX, priceAsset: Currency.KETH },
+            { amountAsset: Currency.VEX, priceAsset: Currency.KBCH },
+            { amountAsset: Currency.VEX, priceAsset: Currency.KBTC },
+            // { amountAsset: Currency.VEX, priceAsset: Currency.KWAVES },
+            // { amountAsset: Currency.VEX, priceAsset: Currency.VAI },                                              
         ];
+        // var assetList = [
+        //    Currency.KDEX,
+        //    Currency.IKI,
+        //    Currency.SGD,
+        //    Currency.HOTX,
+        //    Currency.KLTC,
+        //    Currency.KETH,
+        //    Currency.KBCH,
+        //    Currency.KBTC,
+        //    Currency.KWAVES,
+        //    Currency.VAI,
+        //    Currency.VEX
+        // ];
 
-        var i = 0, j = 0, k = 0;
+        // var i = 0, j = 0, k = 0;
 
-        var favoritePairs = [];
+        // var favoritePairs = [];
         
-        for (i = 0; i < assetList.length; i++){
-            for (j = 0; j < assetList.length; j++) {
-                if(assetList[i] != assetList[j]) {
-                    favoritePairs[k] = {amountAsset: assetList[i], priceAsset: assetList[j]};
-                    k++;                             
-                }                           
-            }
-        }
+        // for (i = 0; i < assetList.length; i++){
+        //     for (j = 0; j < assetList.length; j++) {
+        //         if(assetList[i] != assetList[j]) {
+        //             favoritePairs[k] = {amountAsset: assetList[i], priceAsset: assetList[j]};
+        //             k++;                             
+        //         }                           
+        //     }
+        // }
 
         ctrl.favoritePairs = favoritePairs;
 
